59fe3f6ae36d553484c33274a7abd094d9f882b7
9570075 <9570075@qq.com>
2020-06-21 11:07:53 +0800

    Series Done

--- series/Series.cs
+++ series/Series.cs
@@ -4,11 +4,11 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        // throw new NotImplementedException("You need to implement this function.");
         
-
+        if (sliceLength > numbers.Length || sliceLength <= 0 ) throw new ArgumentException();
 
         string[] res = new string[numbers.Length - sliceLength + 1];
+        
         for (int i = 0; i < numbers.Length - sliceLength + 1; i++)
         {
             res[i] = numbers.Substring(i, sliceLength);
